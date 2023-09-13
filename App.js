8b9531efd030bbbf6b0dedb2d27a029d309db655
lipestile <felipebalbino53@gmail.com>
2023-09-13 17:50:32 -0300

    css

--- App.js
+++ App.js
@@ -1,10 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Alert, Button, FlatList, FlatListComponent, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [seconds, setseconds] = useState(0)
+  const [minutes, setminutes] = useState(0)
+  const [custominteval, setcustominterval] = useState();
+  const [estado, setestado] = useState([])
+
+  const startTime = () => {
+    setcustominterval(
+      setInterval(() => {
+        changeTime();
+      }, 1000)
+    );
+  };
+  const stopTimer = () => {
+    if (custominteval) {
+      clearInterval(custominteval);
+      var sal = 'Minuto Pausado  ' + minutes + ':' + seconds
+      var valor = [...estado]
+      valor.unshift(sal)
+      setestado(valor)
+      console.log(estado)
+
+    }
+  };
+
+  const clear = () => {
+    stopTimer();
+    setseconds(0);
+    setminutes(0);
+    setestado([]);
+  };
+  const changeTime = () => {
+    setseconds((prevState) => {
+      if (prevState + 1 == 60) {
+        setminutes(minutes + 1);
+        return 0;
+      }
+      return prevState + 1;
+    })
+
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textTimer}>
+        {minutes < 10 ? '0' + minutes : minutes}:
+        {seconds < 10 ? '0' + seconds : seconds}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title='Start' onPress={startTime} />
+        <Button title='Stop' onPress={stopTimer} />
+        <Button title='Reset' onPress={clear} />
+      </View>
+        <FlatList
+          data={estado}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item}</Text>
+            </View>
+          )}
+          style={{height: 30}}
+        />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +76,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 100
   },
+  textTimer: {
+    fontSize: 30,
+
+  },
+  buttonContainer: {
+    width: '50%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    marginTop: 20,
+  }
 });
